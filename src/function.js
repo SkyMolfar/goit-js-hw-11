e57c589e83d59f49c22e2470fb2912f7c8481533
SkyMolfar <skymolfars@gmail.com>
2023-05-29 19:53:13 +0200

    u

--- src/function.js
+++ src/function.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+
+const MEDIA_API_KEY = '36804181-9c5d5f31163c379def57751b4';
+
 export async function fetchPhotos(query, { key = MEDIA_API_KEY, page = 1, per_page = 20 } = {}) {
     let res = await axios(`https://pixabay.com/api/?key=${key}&q=${query}&orientation=horizontal&safesearch=true&image_type=photo&page=${page}&per_page=${per_page}`);
     if (res.status !== 200) {
